1bade68888c37e4743be5f2217f5b956eef25539
Jim Shaw <jimxshaw@gmail.com>
2019-12-08 14:51:08 -0600

    Re-write fetchPosts to use thunk.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,9 @@
 import jsonPlaceholder from "../apis/jsonPlaceholder";
 
-export const fetchPosts = async () => {
+export const fetchPosts = () => {
+  return async function(dispatch, getState) {
     const response = await jsonPlaceholder.get("/posts");
 
-    return {
-        type: "FETCH_POSTS",
-        payload: response
-    };
+    dispatch({ type: "FETCH_POSTS", payload: response });
+  };
 };
